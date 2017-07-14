6b397b92bb4477948ed113997bab8cba05e0049d
Anayn Pavez <nanyn.pavez@gmail.com>
2017-07-14 11:53:33 -0300

    validación inicial realizada

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,45 @@
 $(document).ready(function() {
 	$(".button-collapse").sideNav();
 	$('.collapsible').collapsible();
+	
+	api();
+
+	$('#btn-inicio').click(function(e) {
+		e.preventDefault();
+		validarCorreo();
+		validarContra();
+
+		if(validarCorreo() && validarContra()){
+			window.open('opciones.html','_self',false);
+			localStorage.email = email;
+		}
+	});
 });
+
+
+var validarCorreo = function(){
+	var email = $('#icon_prefix').val();
+	var validarEmail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/.test(email);
+
+	if(!validarEmail){
+		$('#icon_prefix').append(alert('Debes ingresar un correo valido main@domain.com'));
+		return false;
+	}
+	else{
+		return true;	
+	}	
+};
+
+var validarContra= function(){
+	var password = $('#icon_telephone').val();
+	var validarPass= (/[0-8]+/).test(password);
+
+	if (!validarPass){
+		$('#icon_telephone').append(alert('Debes ingresar una contraseña válida de 8 dígitos'));
+		return false;
+	}else{
+		return true;
+	}
+};
+
 
